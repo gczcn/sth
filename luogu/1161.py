7812a6a19d8c2b197ab94d697b8a3b1391d617a6
gczcn <494353540@qq.com>
2025-05-11 01:11:37 +0800

    update

--- luogu/1161.py
+++ luogu/1161.py
@@ -1,13 +1,20 @@
-# n, d = int(input()), []
-# for i in range(n):
-#     l = input().split()
-#     b = float(l[0])
-#     for j in range(1, int(l[1]) + 1):
-#         a = int(j * b)
-#         if a in d: d.remove(a)
-#         else: d.append(a)
-# print(d[0])
-n, l = int(input()), []
-for i in range(n):
-    l.append(input().split())
+# Need PyPy 3
+n, r = int(input()), [False for _ in range(2000001)]
+for j in range(n):
+    l = input().split()
+    for i in range(1, int(l[1]) + 1):
+        r[int(i * float(l[0]))] = not r[int(i * float(l[0]))]
+print(r.index(True))
 
+# Deepseek:
+# n = int(input())
+# result = 0
+#
+# for _ in range(n):
+#     a_str, b = input().split()
+#     a = float(a_str)
+#     b = int(b)
+#     for i in range(1, b + 1):
+#         result ^= int(i * a)
+#
+# print(result)
---
+++ luogu/1876.py
@@ -0,0 +1,4 @@
+N, i = int(input()), 2
+print(1, end = '')
+while i * i <= N:
+    print(' ' + str(i * i), end = ''); i += 1

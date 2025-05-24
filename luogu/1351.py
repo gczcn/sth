0a251606589a47380fd95cc6eaeca04ca56d2b5d
gczcn <494353540@qq.com>
2025-05-24 23:39:10 +0800

    update

--- luogu/1351.py
+++ luogu/1351.py
@@ -1,16 +1,13 @@
-n, l, t, lst = int(input()), {}, 0, 0
+n, t, lst = int(input()), 0, 0
+l = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
-    i = input().split()
-    u, v = int(i[0]), int(i[1])
-    l[u] = l.get(u, []) + [v]
-    l[v] = l.get(v, []) + [u]
-W = list(map(int, input().split()))
+    u, v = map(int, input().split())
+    l[u].append(v)
+    l[v].append(u)
+W = [0] + list(map(int, input().split()))
 for i in range(1, n + 1):
-    for j in l[i]:
-        if j in l:
-            for k in l[j]:
-                if k > i:
-                    c = W[i - 1] * W[k - 1]
-                    t += 2 * c
-                    if c > lst: lst = c
-print(lst, t % 10007)
+    li = l[i]
+    if len(li) >= 2:
+        lst = max(lst, max([W[j] for j in li]) * sorted([W[j] for j in li])[-2])
+        t = (t + (sum([W[j] for j in li]) ** 2 - sum([W[j] ** 2 for j in li]))) % 10007
+print(lst, t)

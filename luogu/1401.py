445e9a416220e1ca1b2fef04be0b1386daa037e0
gczcn <494353540@qq.com>
2025-05-25 17:00:52 +0800

    updated P1401

--- luogu/1401.py
+++ luogu/1401.py
@@ -1,10 +1,4 @@
 xl, xu = map(int, input().split())
 yl, yu = map(int, input().split())
-if \
-    -2147483648 <= xl * yl <= 2147483647 and \
-    -2147483648 <= xu * yu <= 2147483647 and \
-    -2147483648 <= xl * yu <= 2147483647 and \
-    -2147483648 <= xu * yl <= 2147483647:
-    print('int')
-else:
-    print('long long int')
+l = [xl * yl, xu * yu, xl * yu, xu * yl]
+print('int' if min(l) >= -2147483648 and max(l) <= 2147483647 else 'long long int')
